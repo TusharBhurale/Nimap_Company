97c3ccf224172af39466e56207be01d67ab74d85
Tushar Bhurale <bhuraletusharcse@gmail.com>
2024-11-21 13:18:32 +0530

    Commit successfully

---
+++ src/main/java/com/NimapCompany/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.NimapCompany.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.NimapCompany.entity.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
